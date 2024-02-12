da3b5b27c980939332c882164773fb0292fb4d9f
ColinKammer <colin.kammer@gmx.de>
2024-02-12 23:01:21 +0100

    Fix compilation of PicewiseLERP

--- src/lerp.h
+++ src/lerp.h
@@ -1,6 +1,15 @@
 #pragma once
 
-namespace cjk {
+#include <algorithm>
+#include <array>
+
+namespace cjk::lerp {
+
+template <typename InT, typename OutT>
+struct Point {
+    InT x;
+    OutT y;
+};
 
 template <typename InT, typename OutT = InT>
 class LERP {
@@ -13,6 +22,12 @@ class LERP {
     //   = (y1*x2/y2 - x1 + x) * (y2/x2)
     // y = (d + x) * m; d = x2*y1/y2; m = y2/x2
 public:
+    constexpr LERP()
+        : m_d(0)
+        , m_m(1)
+    {
+    }
+
     constexpr LERP(
         const InT& x1, const OutT& y1,
         const InT& x2, const OutT& y2)
@@ -20,10 +35,71 @@ public:
         , m_m(y2 / x2)
     {
     }
+    constexpr LERP(
+        const Point<InT, OutT>& p1,
+        const Point<InT, OutT>& p2)
+        : m_d(p2.x * p1.y / p2.y )
+        , m_m(p2.y / p2.x)
+    {
+    }
 
     [[nodiscard]] Promoted constexpr operator()(const InT& x) const
     {
         return (m_d + x) * m_m;
+    }
+
+    [[nodiscard]] LERP<OutT, InT> GetInverse() const
+    {
+        // y = (d + x) * m
+        // x = y / m - d
+        //   = (-d*m + y) * 1/m
+        LERP<OutT, InT> ret;
+        ret.m_d = -m_d * m_m;
+        ret.m_m = 1 / m_m;
+    }
+};
+
+template <typename InT, typename OutT = InT, unsigned NumPoints = 2>
+class PicewiseLERP {
+    using Promoted = decltype(InT {} + OutT {});
+    using PointList = std::array<Point<InT, OutT>, NumPoints>;
+
+    PointList m_points;
+
+public:
+    constexpr PicewiseLERP(const PointList& points_sorted)
+        : m_points(points_sorted)
+    {
+    }
+
+    [[nodiscard]] Promoted constexpr operator()(const InT& x) const
+    {
+        if (x < m_points[0].x) {
+            LERP<InT, OutT> lerp(m_points[0], m_points[1]);
+            return lerp(x);
+        }
+
+        for (size_t i = 1; i < NumPoints; i++) {
+            if (m_points[i - 1] <= x && x <= m_points[i]) {
+                LERP<InT, OutT> lerp(m_points);
+                return lerp(x);
+            }
+        }
+
+        LERP<InT, OutT> lerp(m_points[NumPoints - 2], m_points[NumPoints - 1]);
+        return lerp(x);
+    }
+
+    [[nodiscard]] PicewiseLERP<OutT, InT, NumPoints> GetInverse() const
+    {
+        PicewiseLERP<OutT, InT, NumPoints> ret;
+        for (size_t i = 0; i < NumPoints; i++) {
+            ret.m_points[i].x = m_points[i].y;
+            ret.m_points[i].y = m_points[i].x;
+        }
+        // sort is required for non monotonic functions
+        std::sort(ret.m_points.begin(), ret.m_points.end());
+        return ret;
     }
 };
 
