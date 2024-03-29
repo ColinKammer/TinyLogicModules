ab8e95aca6db8fb599ec8144da152ed59ee25aa8
ColinKammer <colin.kammer@gmx.de>
2024-03-29 12:59:30 +0100

    Fix Bugs in LERP (found by UnitTest)

--- src/lerp.h
+++ src/lerp.h
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <array>
+#include <stdexcept>
 
 namespace cjk::lerp {
 
@@ -14,48 +15,39 @@ struct Point {
 template <typename InT, typename OutT = InT>
 class LERP {
     using Promoted = decltype(InT {} + OutT {});
-    Promoted m_d;
     Promoted m_m;
+    Promoted m_b;
 
-    // (x - x1) / x2 = (y - y1) / y2
-    // y = (x - x1) * (y2/x2) + y1
-    //   = (y1*x2/y2 - x1 + x) * (y2/x2)
-    // y = (d + x) * m; d = x2*y1/y2; m = y2/x2
 public:
     constexpr LERP()
-        : m_d(0)
-        , m_m(1)
+        : m_m(1)
+        , m_b(0)
     {
     }
 
-    constexpr LERP(
-        const InT& x1, const OutT& y1,
-        const InT& x2, const OutT& y2)
-        : m_d(x2 * y1 / y2)
-        , m_m(y2 / x2)
-    {
-    }
     constexpr LERP(
         const Point<InT, OutT>& p1,
         const Point<InT, OutT>& p2)
-        : m_d(p2.x * p1.y / p2.y)
-        , m_m(p2.y / p2.x)
+        : m_m((p1.y - p2.y) / (p1.x - p2.x))
+        , m_b(p1.y - p1.x * m_m)
     {
+        if (p1.x == p2.x)
+            throw std::invalid_argument("Conflicting Points");
     }
 
     [[nodiscard]] Promoted constexpr operator()(const InT& x) const
     {
-        return (m_d + x) * m_m;
+        return m_m * x + m_b;
     }
 
-    [[nodiscard]] LERP<OutT, InT> GetInverse() const
+    [[nodiscard]] LERP<OutT, InT> constexpr GetInverse() const
     {
-        // y = (d + x) * m
-        // x = y / m - d
-        //   = (-d*m + y) * 1/m
+        if (m_m == 0)
+            throw std::runtime_error("No inverse Function possible - it would not be a function");
         LERP<OutT, InT> ret;
-        ret.m_d = -m_d * m_m;
         ret.m_m = 1 / m_m;
+        ret.m_b = (-1) * (m_b / m_m);
+        return ret;
     }
 };
 
@@ -64,12 +56,26 @@ class PicewiseLERP {
     using Promoted = decltype(InT {} + OutT {});
     using PointList = std::array<Point<InT, OutT>, NumPoints>;
 
+    static_assert(NumPoints >= 2, "Can't inter-/extrapolate using less than 2 Points");
+
     PointList m_points;
 
 public:
+    constexpr PicewiseLERP()
+    {
+        m_points[0] = { 0, 0 };
+        m_points[1] = { 1, 1 };
+    }
+
     constexpr PicewiseLERP(const PointList& points_sorted)
         : m_points(points_sorted)
     {
+        InT lastX = m_points[0].x;
+        for (size_t i = 1; i < NumPoints; i++) {
+            if (m_points[i].x < lastX)
+                throw std::runtime_error("Points are not sorted");
+            lastX = m_points[i].x;
+        }
     }
 
     [[nodiscard]] Promoted constexpr operator()(const InT& x) const
@@ -90,15 +96,22 @@ public:
         return lerp(x);
     }
 
-    [[nodiscard]] PicewiseLERP<OutT, InT, NumPoints> GetInverse() const
+    [[nodiscard]] PicewiseLERP<OutT, InT, NumPoints> constexpr GetInverse() const
     {
+        {
+            OutT lastY = m_points[0].y;
+            for (size_t i = 1; i < NumPoints; i++) {
+                if (m_points[i].y <= lastY)
+                    throw std::runtime_error("No inverse Function possible - it would not be a function");
+                lastY = m_points[i].y;
+            }
+        }
+
         PicewiseLERP<OutT, InT, NumPoints> ret;
         for (size_t i = 0; i < NumPoints; i++) {
             ret.m_points[i].x = m_points[i].y;
             ret.m_points[i].y = m_points[i].x;
         }
-        // sort is required for non monotonic functions
-        std::sort(ret.m_points.begin(), ret.m_points.end());
         return ret;
     }
 };
